efbddfbf1feb16ace62f19e1ffe76185c04009e6
vafo <v.safokhujaev@student.inha.uz>
2023-04-25 14:49:10 +0500

    task complete
    Closes #5

--- estc_service.c
+++ estc_service.c
@@ -37,31 +37,51 @@
 #include "ble_gatts.h"
 #include "ble_srv_common.h"
 
+ble_uuid128_t base_uuid = {
+    .uuid128 = ESTC_BASE_UUID
+};
+
 static ret_code_t estc_ble_add_characteristics(ble_estc_service_t *service);
 
 ret_code_t estc_ble_service_init(ble_estc_service_t *service)
 {
+    VERIFY_PARAM_NOT_NULL(service);
     ret_code_t error_code = NRF_SUCCESS;
-    // TODO: 4. Add service UUIDs to the BLE stack table using `sd_ble_uuid_vs_add`
-    // TODO: 5. Add service to the BLE stack using `sd_ble_gatts_service_add`
+    ble_uuid_t service_uuid = {
+        .uuid = ESTC_SERVICE_UUID
+    };
 
-    // NRF_LOG_DEBUG("%s:%d | Service UUID: 0x%04x", __FUNCTION__, __LINE__, service_uuid.uuid);
-    // NRF_LOG_DEBUG("%s:%d | Service UUID type: 0x%02x", __FUNCTION__, __LINE__, service_uuid.type);
-    // NRF_LOG_DEBUG("%s:%d | Service handle: 0x%04x", __FUNCTION__, __LINE__, service->service_handle);
+    // TODO: 4. Add service UUIDs to the BLE stack table using `sd_ble_uuid_vs_add`
+    error_code = sd_ble_uuid_vs_add(&base_uuid, &service_uuid.type);
+    APP_ERROR_CHECK(error_code);
+
+    // TODO: 5. Add service to the BLE stack using `sd_ble_gatts_service_add`
+    error_code = sd_ble_gatts_service_add(BLE_GATTS_SRVC_TYPE_PRIMARY, &service_uuid, &service->service_handle);
+    APP_ERROR_CHECK(error_code);
+
+    NRF_LOG_DEBUG("%s:%d | Service UUID: 0x%04x", __FUNCTION__, __LINE__, service_uuid.uuid);
+    NRF_LOG_DEBUG("%s:%d | Service UUID type: 0x%02x", __FUNCTION__, __LINE__, service_uuid.type);
+    NRF_LOG_DEBUG("%s:%d | Service handle: 0x%04x", __FUNCTION__, __LINE__, service->service_handle);
 
     return estc_ble_add_characteristics(service);
 }
 
 static ret_code_t estc_ble_add_characteristics(ble_estc_service_t *service)
 {
+    VERIFY_PARAM_NOT_NULL(service);
     ret_code_t error_code = NRF_SUCCESS;
+    ble_uuid_t char_uuid = {
+        .uuid = ESTC_GATT_CHAR_1_UUID
+    };
 
     // TODO: 6.1. Add custom characteristic UUID using `sd_ble_uuid_vs_add`, same as in step 4
-
+    error_code = sd_ble_uuid_vs_add(&base_uuid, &char_uuid.type);
+    APP_ERROR_CHECK(error_code);
 
     // TODO: 6.5. Configure Characteristic metadata (enable read and write)
     ble_gatts_char_md_t char_md = { 0 };
-
+    char_md.char_props.read = 1;
+    char_md.char_props.write = 1;
 
     // Configures attribute metadata. For now we only specify that the attribute will be stored in the softdevice
     ble_gatts_attr_md_t attr_md = { 0 };
@@ -69,16 +89,21 @@ static ret_code_t estc_ble_add_characteristics(ble_estc_service_t *service)
 
 
     // TODO: 6.6. Set read/write security levels to our attribute metadata using `BLE_GAP_CONN_SEC_MODE_SET_OPEN`
-
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
 
     // TODO: 6.2. Configure the characteristic value attribute (set the UUID and metadata)
     ble_gatts_attr_t attr_char_value = { 0 };
-
+    attr_char_value.p_attr_md = &attr_md;
+    attr_char_value.p_uuid = &char_uuid;
 
     // TODO: 6.7. Set characteristic length in number of bytes in attr_char_value structure
-
+    attr_char_value.init_len = sizeof(uint16_t);
+    attr_char_value.max_len = sizeof(uint16_t);
 
     // TODO: 6.4. Add new characteristic to the service using `sd_ble_gatts_characteristic_add`
+    error_code = sd_ble_gatts_characteristic_add(service->service_handle, &char_md, &attr_char_value, &service->char_1);
+    APP_ERROR_CHECK(error_code);
 
     return NRF_SUCCESS;
 }
